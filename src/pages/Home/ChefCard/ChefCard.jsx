211173ec5cca14f65ca6fbc2c11074fa8942460b
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-05-03 23:11:35 +0600

    used lazy load on chef card image

--- src/pages/Home/ChefCard/ChefCard.jsx
+++ src/pages/Home/ChefCard/ChefCard.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import './ChefCard.css'
 import { FaArrowRight } from 'react-icons/fa';
+import LazyLoad from 'react-lazy-load';
 import { Link } from 'react-router-dom';
 
 const ChefCard = ({ chef }) => {
@@ -8,7 +10,9 @@ const ChefCard = ({ chef }) => {
     return (
         <div className="flex items-center gap-5 group w-full p-6 bg-white border border-gray-200 rounded-lg shadow">
             <div className='w-[170px] h-[170px] relative bg-violet-500 rounded-full'>
-                <img className='w-[200px] h-[200px] absolute bottom-0 rounded-full' src={chef_picture} alt="" />
+                <LazyLoad height={200} width={200} offset={100}>
+                    <img className='w-[200px] h-[200px] absolute bottom-0 rounded-full' src={chef_picture} alt="" />
+                </LazyLoad>
             </div>
             <div className='text-left space-y-2'>
                 <h4 className='font-bold text-3xl font-bubblegum'>{chef_name}</h4>
